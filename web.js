5e3d63325493a036ccd01d03bfe6efc5059a57e0
Isha Deshpande <deshpande.isha@gmail.com>
2013-07-12 05:58:59 +0000

    Using index.html data in web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fileName = "Index.html";
+
+var outputData = fs.readFileSync(fileName, encoding);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(outputData);
 });
 
 var port = process.env.PORT || 5000;
